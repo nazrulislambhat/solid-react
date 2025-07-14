b1b58ee9077472e6928589d196a66fd02ce71e1f
Nazrul Islam <nazrulislambhat@gmail.com>
2025-07-14 07:00:27 +0530

    Some Changes

--- hooks/RefHook.jsx
+++ hooks/RefHook.jsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import { useRef, useState } from 'react';
-function RefHook() {
-  const timerRef = useRef(null);
-  const [count, setCount] = useState(0);
-  const startTimer = () => {
-    if (!timerRef.current) {
-      timerRef.current = setInterval(() => {
-        setCount((prevCount) => prevCount + 1);
-      });
-    }
-  };
-  const stopTimer = () => {
-    clearInterval(timerRef.current);
-    timerRef.current = null;
-  };
+// import { useRef, useEffect, useState } from 'react';
+
+// function RefHook() {
+//   const [count, setCount] = useState(0);
+//   const prevCount = useRef(0);
+
+//   useEffect(() => {
+//     prevCount.current = count;
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <h1>Current Count:{count}</h1>
+//       <h2>Previous Count: {prevCount.current}</h2>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
+//     </div>
+//   );
+// }
+
+// export default RefHook;
+// import { useRef } from 'react';
+
+// function RefHook() {
+//   const timeoutRef = useRef(null);
+
+//   const handleChange = (e) => {
+//     clearTimeout(timeoutRef.current);
+//     timeoutRef.current = setTimeout(() => {
+//       console.log('Debounced Value:', e.target.value);
+//     }, 500);
+//   };
+//   return <input onChange={handleChange} placeholder="Search..." />;
+// }
+
+// export default RefHook;
+import { useState, useRef } from 'react';
+
+export default function UseRefHook() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef = useRef(null);
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-    </div>
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
   );
 }
-
-export default RefHook;
